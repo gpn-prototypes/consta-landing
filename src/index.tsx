861ab7c19fe92d7f4300ad643abfc512bb6379d3
gizeasy <gizeasy@gmail.com>
2020-11-13 13:52:37 +0300

    w

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+import App from "./App";
+import { context } from "@reatom/react";
+import { store } from "./modules/app";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Root: React.FC = () => {
+  return (
+    <context.Provider value={store}>
+      <App />
+    </context.Provider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
